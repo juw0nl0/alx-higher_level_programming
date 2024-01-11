311be264a9d2b9ce63ba23b54a12bc9ea34de98c
juw0nl0 <juwonoluwaseun@gmail.com>
2024-01-11 07:21:44 -0800

    correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t count = 0;
 
-	for (i = 0; h != NULL; i++)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n ", h->n);
 		h = h->next;
 	}
-	return (i);
+	count++;
+	return (count);
 }
