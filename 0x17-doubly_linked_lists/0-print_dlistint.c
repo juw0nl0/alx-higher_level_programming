fd7fc3670d2f69c8ae1e75bc74431e17886aa954
juw0nl0 <juwonoluwaseun@gmail.com>
2024-01-11 07:15:58 -0800

    modify script 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,16 +9,12 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *ptr = h;
-	size_t num = 0;
+	size_t i;
 
-	while (ptr && ptr -> prev)
-		ptr = ptr -> prev;
-	while (ptr)
+	for (i = 0; h != NULL; i++)
 	{
-		printf("%d\n", ptr->num);
-		num++;
-		ptr = ptr->next;
+		printf("%d\n ", h->n);
+		h = h->next;
 	}
-	return (num);
+	return (i);
 }
