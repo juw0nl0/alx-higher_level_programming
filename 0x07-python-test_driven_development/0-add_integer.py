a1f21b1e4e3c653dc033fdf4a237f245832cac9e
juw0nl0 <juwonoluwaseun@gmail.com>
2024-01-29 07:21:06 -0800

    added script 3-4

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """A function that add two(2) numbers together"""
+
+    if not a or (type(a) != int and type(a) != float):
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+
+    return(int(a) + int(b))
