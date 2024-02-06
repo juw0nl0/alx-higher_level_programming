d5c50c925358aa35d74696824a76ad987a06d96f
juw0nl0 <juwonoluwaseun@gmail.com>
2024-02-06 09:13:47 -0800

    added script 3-6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Representation of base geometry."""
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """Representation of base geometry."""
+
+    def area(self):
+        """function not implemented."""
+        raise Exception("area() is not implemented")
