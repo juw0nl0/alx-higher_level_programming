c1a01eb462542e1187e3057bac0aeb880cae31e9
juw0nl0 <juwonoluwaseun@gmail.com>
2024-02-06 05:41:36 -0800

    added script 9-12

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a function that returns the dictionary description with 
+"""a function that returns the dictionary description with
 simple data structure (list, dictionary, string, integer
 and boolean) for JSON serialization of an object
 """
